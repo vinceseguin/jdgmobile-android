7a41979eea80478992c127e092f4dcf1b0e6c829
vinnyduke <vincent.gentilcore@gmail.com>
2014-07-11 15:31:38 -0400

    Ajout des vues Partenaire et Apropos...

--- src/ca/qc/jeuxdegenie/jdgmobile/view/AProposFragment.java
+++ src/ca/qc/jeuxdegenie/jdgmobile/view/AProposFragment.java
@@ -1,14 +1,33 @@
 package ca.qc.jeuxdegenie.jdgmobile.view;
 
+import ca.qc.jeuxdegenie.jdgmobile.R;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class AProposFragment extends Fragment {
 
+	/* (non-Javadoc)
+	 * @see android.app.ListFragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+	 */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 		
+		return inflater.inflate(R.layout.apropos_fragment, container, false);
+	}
+	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
 		
+		WebView wv = (WebView) getActivity().findViewById(R.id.apropos);
+		WebSettings settings = wv.getSettings();
+		settings.setDefaultTextEncodingName("utf-8");
+		wv.loadUrl("file:///android_asset/www/about/about.html");
 	}
 }
--- src/ca/qc/jeuxdegenie/jdgmobile/view/PartenaireFragment.java
+++ src/ca/qc/jeuxdegenie/jdgmobile/view/PartenaireFragment.java
@@ -1,7 +1,30 @@
 package ca.qc.jeuxdegenie.jdgmobile.view;
 
+import ca.qc.jeuxdegenie.jdgmobile.R;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 
 public class PartenaireFragment extends Fragment {
 
+	/* (non-Javadoc)
+	 * @see android.app.ListFragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+	 */
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		return inflater.inflate(R.layout.partenaire_fragment, container, false);
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		
+		WebView wv = (WebView) getActivity().findViewById(R.id.partenaires);
+		wv.loadUrl("file:///android_asset/www/partners/partners.html");
+	}
 }
