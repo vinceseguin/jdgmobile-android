d9e8998d31ad6cb57ace79abe945a294b91fd979
patrickmcanany <patrick.mcanany@gmail.com>
2014-07-23 17:13:00 -0400

    Offline viewing fix with comments

--- src/ca/qc/jeuxdegenie/jdgmobile/controller/SqLiteDatabaseHelper.java
+++ src/ca/qc/jeuxdegenie/jdgmobile/controller/SqLiteDatabaseHelper.java
@@ -32,12 +32,14 @@ public class SqLiteDatabaseHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		if(newVersion != DataAccessFacade.getOfflineVersionNumber()) { 
+		//user is simply offline now
+		if(newVersion == DataAccessFacade.getOfflineVersionNumber()) { 
+			//this doesn't seem to update the SCHEMA_VERSION (I guess that's only updated when we call the parent ctr : super(..))
+			db.setVersion(oldVersion); // doesn't affect the fact that when the user reopens the app at a later time, it still thinks its at Integer.MAX_VALUE
+		} else { 
 			db.execSQL(sqlDropEventTable);
 			db.execSQL(sqlDropResultTable);
 			onCreate(db);
-		} else { 
-			db.setVersion(oldVersion);
 		}
 	}
 	
@@ -47,10 +49,20 @@ public class SqLiteDatabaseHelper extends SQLiteOpenHelper {
 		// Database will always be downgraded on creation because we pass 1 everytime.
 		// We need to do this because we have to check in the database for the db_version value.
 		// If database is outdated, tables will be recreated manually.
+		
+		
+		//user was offline, don't drop everything.. but there's a risk he's missing updates
 		if(oldVersion != DataAccessFacade.getOfflineVersionNumber()) { 
 			db.execSQL(sqlDropEventTable);
 			db.execSQL(sqlDropResultTable);
 			onCreate(db);
 		}
+		//alternatively, if we always drop tables here, the user might be pulling the new data without any actual updates
+		//if the db.setVersion(oldVersion) worked in the onUpgrade, onDowngrade wouldn't even be called after going back online.. unless there was a real new version..
+		/*
+		db.execSQL(sqlDropEventTable);
+		db.execSQL(sqlDropResultTable);
+		onCreate(db);
+		*/
 	}
 }
